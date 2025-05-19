57ceea32e6076abd7c7420f267ea4397ed76dea6
liangyouyi <liangyouyi@jinsanhui.com>
2025-05-19 09:36:24 +0800

    同步

---
+++ typescript/a.ts
@@ -0,0 +1,3 @@
+const timer = setTimeout(() => {
+  console.log('Timed out');
+}, 2000);
--- 草纸/a.js
+++ 草纸/a.js
@@ -1,11 +1,19 @@
-function a () {
-    return new Promise(resolve => resolve()).then(e =>{
-        console.log(1)
-    })
+const getDayCountOfMonth = function(year, month) {
+    return new Date(year, month + 1, 0).getDate();
+};
+const siblingMonth = function(src, diff) {
+    const temp = new Date(src); // lets copy it so we don't change the original
+ 
+    const newMonth = temp.getMonth() + diff;
+    const newMonthDayCount = getDayCountOfMonth(temp.getFullYear(), newMonth);
+    if (newMonthDayCount < temp.getDate()) {
+        temp.setDate(newMonthDayCount);
+    }
+    temp.setMonth(newMonth);
+    console.log(newMonth)
+    return temp;
+};
 
-}
-function b(){
-    a()
-    console.log(2)
-}
-b()
+const date = new Date('2025-3-30');
+siblingMonth(date,-1)
+// console.log()
